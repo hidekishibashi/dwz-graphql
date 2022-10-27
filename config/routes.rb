d647fe8dba0c9921cb27c001b46485bf219f25c9
hidekishibashi <dev.hidekishibashi@gmail.com>
2022-10-28 03:07:13 +0900

    add: routes of graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 end
